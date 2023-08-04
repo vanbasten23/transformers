1a9c4cf114757ebccfdad1860c208773f7baab2c
Jon Bolin <jonbolin@google.com>
2023-08-04 18:52:07 +0000

    Disable checkpoint writing

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -2822,6 +2822,8 @@ class Trainer:
             self.push_to_hub(commit_message="Model save")
 
     def _save_tpu(self, output_dir: Optional[str] = None):
+        #TODO(jonbolin): Re-enable
+        return
         output_dir = output_dir if output_dir is not None else self.args.output_dir
         logger.info(f"Saving model checkpoint to {output_dir}")
 
@@ -2850,6 +2852,8 @@ class Trainer:
             self.tokenizer.save_pretrained(output_dir)
 
     def _save(self, output_dir: Optional[str] = None, state_dict=None):
+        #TODO(jonbolin): Re-enable
+        return
         # If we are executing this function, we are the process zero, so we don't check for that.
         output_dir = output_dir if output_dir is not None else self.args.output_dir
         os.makedirs(output_dir, exist_ok=True)
