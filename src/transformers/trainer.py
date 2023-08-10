de60ee95571160041ffc90c67abdfbe59dac2ec7
Siyuan Liu <lsiyuan@google.com>
2023-08-10 23:46:26 +0000

    Update dataloader to load entire dataset for each host

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -840,7 +840,7 @@ class Trainer:
             dataloader_params["drop_last"] = self.args.dataloader_drop_last
             dataloader_params["worker_init_fn"] = seed_worker
 
-        return self.accelerator.prepare(DataLoader(train_dataset, **dataloader_params))
+        return DataLoader(train_dataset, **dataloader_params)
 
     def _get_eval_sampler(self, eval_dataset: Dataset) -> Optional[torch.utils.data.Sampler]:
         # Deprecated code
