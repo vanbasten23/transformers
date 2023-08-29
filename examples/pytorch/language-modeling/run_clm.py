c4878f0f62a60d3dd82c4377135105559875a55c
Jon Bolin <jonbolin@google.com>
2023-08-29 00:34:13 +0000

    Remove defer-init flag

--- examples/pytorch/language-modeling/run_clm.py
+++ examples/pytorch/language-modeling/run_clm.py
@@ -207,14 +207,6 @@ class ModelArguments:
             )
         },
     )
-    spmd_defer_init: bool = field(
-        default=False,
-        metadata={
-            "help": (
-                "Use defer init",
-            )
-        },
-    )
 
     def __post_init__(self):
         if self.config_overrides is not None and (self.config_name is not None or self.model_name_or_path is not None):
